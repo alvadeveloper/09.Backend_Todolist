1eb204597b668fff9f8077a954ffaa830bd78799
alvahuiwork@gmail.com <alvahuiwork@gmail.com>
2020-10-27 21:31:13 +0800

    10-27

---
+++ .bashrc
@@ -0,0 +1,9 @@
+export client_id='2MdOSeT1x6aGKdtQPfPGbC9i6r99Ihoq'
+export client_secret='HgWL9zAMTlHEfwrRZtVcNYP07EMejOW_xprPnjWR_dGdqvLVpGMcyBN_dDHzws_B'
+export api_base_url='https://dev-rp5hh-6z.auth0.com'
+export access_token_url='https://dev-rp5hh-6z.auth0.com/oauth/token'
+export authorize_url='https://dev-rp5hh-6z.auth0.com/authorize'
+export SECRET_KEY='$R\x87\xa3\xaa\x0eMM\xb6_\x89=,\xd0t\x07\xe0\x18\x95\x9a8|7?'
+export DATABASE_URL="postgres://upjybpwdpimwfo:f317b2fa3e2fb8f967e26359c3dbe520d2ca4d532bd5993bb8ed38fe394dc063@ec2-34-234-185-150.compute-1.amazonaws.com:5432/dbalhldorae4u5"
+export REDIRECT_URL='https://dev-rp5hh-6z.auth0.com/authorize?audience=todo&response_type=token&client_id=2MdOSeT1x6aGKdtQPfPGbC9i6r99Ihoq&redirect_uri=https://todolist-udacity2020.herokuapp.com/'
+export TEST_API=["eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjFwZ0VGZXgzN21LUVc0YUI4QlRBaCJ9.eyJpc3MiOiJodHRwczovL2Rldi1ycDVoaC02ei5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NWY4MzA0NGRjOWE1NWIwMDZmMTJhN2UyIiwiYXVkIjoidG9kbyIsImlhdCI6MTYwMzY5NTAyMiwiZXhwIjoxNjAzNzAyMjIyLCJhenAiOiIyTWRPU2VUMXg2YUdLZHRRUGZQR2JDOWk2cjk5SWhvcSIsInNjb3BlIjoiIiwicGVybWlzc2lvbnMiOlsiZGVsZXRlOmxpc3QiLCJnZXQ6bGlzdCIsInBhdGNoOmxpc3QiLCJwb3N0Omxpc3QiXX0.V-9M_3hJO0Q_myoRq7UdlPwivFWuXjOSXOqC4J1dF51FwiPCryntS_7KRTv3pS-Zx7ZSRU9hnzJWcT_TCpzVbtMl6a9u62fpoAmydGNiPkdBVH4vBmwSuC7Cb2FYVBWiLrjxCuOTBHBMVkWpFMbGWdXfk8z4TkdTJMasv9fVVJcKs0-VzWF5oA4DUcBWHjxj5MCfcB5B0_-a6yQM-Sbxv-APceo1Rx1D-NBEn7wvwAxlxNGjWIeb3_ZpEXVaM980XiBNnN8bzPCw4mey27UQ2Snzaf6jsusFLwxqy_r-KURRUlt5HjPGyZzOBwgnHn-WtuYHVqraBxR95NYBadVttg"]
--- test_todo.py
+++ test_todo.py
@@ -9,7 +9,7 @@ from flask_cors import CORS
 
 from models import setup_db, User, Lists, Task
 
-a = ['eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjFwZ0VGZXgzN21LUVc0YUI4QlRBaCJ9.eyJpc3MiOiJodHRwczovL2Rldi1ycDVoaC02ei5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NWY4MzA0NGRjOWE1NWIwMDZmMTJhN2UyIiwiYXVkIjoidG9kbyIsImlhdCI6MTYwMzY5NTAyMiwiZXhwIjoxNjAzNzAyMjIyLCJhenAiOiIyTWRPU2VUMXg2YUdLZHRRUGZQR2JDOWk2cjk5SWhvcSIsInNjb3BlIjoiIiwicGVybWlzc2lvbnMiOlsiZGVsZXRlOmxpc3QiLCJnZXQ6bGlzdCIsInBhdGNoOmxpc3QiLCJwb3N0Omxpc3QiXX0.V-9M_3hJO0Q_myoRq7UdlPwivFWuXjOSXOqC4J1dF51FwiPCryntS_7KRTv3pS-Zx7ZSRU9hnzJWcT_TCpzVbtMl6a9u62fpoAmydGNiPkdBVH4vBmwSuC7Cb2FYVBWiLrjxCuOTBHBMVkWpFMbGWdXfk8z4TkdTJMasv9fVVJcKs0-VzWF5oA4DUcBWHjxj5MCfcB5B0_-a6yQM-Sbxv-APceo1Rx1D-NBEn7wvwAxlxNGjWIeb3_ZpEXVaM980XiBNnN8bzPCw4mey27UQ2Snzaf6jsusFLwxqy_r-KURRUlt5HjPGyZzOBwgnHn-WtuYHVqraBxR95NYBadVttg']
+a = ['eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjFwZ0VGZXgzN21LUVc0YUI4QlRBaCJ9.eyJpc3MiOiJodHRwczovL2Rldi1ycDVoaC02ei5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NWY4MzA0NGRjOWE1NWIwMDZmMTJhN2UyIiwiYXVkIjoidG9kbyIsImlhdCI6MTYwMzgwMzUxOSwiZXhwIjoxNjAzODg5OTE5LCJhenAiOiIyTWRPU2VUMXg2YUdLZHRRUGZQR2JDOWk2cjk5SWhvcSIsInNjb3BlIjoiIiwicGVybWlzc2lvbnMiOlsiZGVsZXRlOmxpc3QiLCJnZXQ6bGlzdCIsInBhdGNoOmxpc3QiLCJwb3N0Omxpc3QiXX0.JU887UjzvnHqnO-WigqKBGOSbN3xUr_HZoJsHlTlfMy4eqkq7TMVENb_Uy_6teyvU-_1ULi34o5sZaOTDg9pOL1uA4tGt-kzXw1AsrkbHKKYYUoeUCHUa7jnqxhRxG4_1e2cMUQfeDLXwNnQzNceLA73LcOP2TZEnGwvmxqi7Wh_-r_0hLGzRrm5YjsrWonZgy9h4S_n8PhNFh1lk_UGrPnYVXZKBUZpYXdehc3cAtrrQ29g0ToojkpZyelH11jwEp4KvrVcbfetm1y9nf4m8jPxg1QK4ffNO4J_4hTmwNQERE8dAHLNR51HoemM_QVmkGoAeVaPFpwL2DYlTBlGeg']
 
 headers = {
     'Authorization': 'Bearer {}'.format(a[0]),
@@ -47,7 +47,7 @@ class TodoTest(unittest.TestCase):
         res = requests.post('https://todolist-udacity2020.herokuapp.com/addtask',
                             json=test_taskdata, headers=headers)
 
-        self.assertEqual(res.status_code, 500)
+        self.assertEqual(res.status_code, 200)
 
 
     def test_patchtask_success(self):
@@ -56,12 +56,14 @@ class TodoTest(unittest.TestCase):
 
         self.assertEqual(res.status_code, 200)
 
-
     def test_deletetask_success(self):
         res = requests.delete(
-            'https://todolist-udacity2020.herokuapp.com/task/delete/8', headers=headers)
+            'https://todolist-udacity2020.herokuapp.com/task/delete/15', headers=headers)
 
         self.assertEqual(res.status_code, 200)
+
+
+
 
 
 # Make the tests conveniently executable
